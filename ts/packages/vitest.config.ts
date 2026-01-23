03b1e88ac4ac4e6e45b420cac17868e3195969eb
Denis Martin Budinski <denis.budinski@intellegens.hr>
2026-01-23 13:30:31 +0100

    Set up github summary

--- ts/packages/vitest.config.ts
+++ ts/packages/vitest.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      provider: "v8",
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
     },
     projects: [
       {

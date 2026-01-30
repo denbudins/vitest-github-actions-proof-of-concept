d59757eafd67924c734ae7c7a8b4b85909212782
Denis Martin Budinski <denis.budinski@intellegens.hr>
2026-01-30 15:31:57 +0100

    Update code style checker

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];

729fd376a352be5b1787d02b6c0c1961016dffc5
Denis Martin Budinski <denis.budinski@intellegens.hr>
2026-02-02 08:22:24 +0100

    Add some warrnings

--- ts/packages/index.ts
+++ ts/packages/index.ts
@@ -1,3 +1,7 @@
+// ESLint warning examples:
+var unusedVar = 123; // 'var' instead of 'let' or 'const', and unused variable
+console.log("Debug info"); // console.log warning
+
 export const sumTwoNumbers = (number1: number, number2: number): number => {
   return number1 + number2;
 };

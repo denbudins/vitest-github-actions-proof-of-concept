70ea0f6dba7fe7d2918cf7d9860ab87b21db8cde
Denis Martin Budinski <denis.budinski@intellegens.hr>
2026-02-16 15:19:14 +0100

    Remove

--- ts/packages/index.ts
+++ ts/packages/index.ts
@@ -1,6 +1,6 @@
 // ESLint warning examples:
 var unusedVar = 123; // 'var' instead of 'let' or 'const', and unused variable
-console.log('Debug info'); // console.log warning
+console.log('Debug info');
 
 export const sumTwoNumbers = (number1: number, number2: number): number => {
   return number1 + number2;
